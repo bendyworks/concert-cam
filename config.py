1dc5c408ba2c731cc3d24de0d5c99dd394ef93c5
Matt Gauger <dev+mathias@bendyworks.com>
2013-07-17 15:22:48 -0500

    Make config class more dynamic

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import json
+import sys
 
 class Config:
-  secret_key = ""
   def load(self, filename):
     config_file = self.load_config_file(filename)
     config = json.loads(config_file)
-    self.secret_key = config["secret_key"]
+    self.dictionary_to_locals(config)
+
+  def dictionary_to_locals(self, definitions):
+    for name, value in definitions.iteritems():
+      setattr(self, name, value)
 
   def load_config_file(self, filename):
     return open(filename, "r+").read()
