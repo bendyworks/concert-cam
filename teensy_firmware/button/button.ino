5a936c92258fc58d85532c0c9a9f626bf78b1770
Matt Gauger <dev+mathias@bendyworks.com>
2013-07-17 14:12:26 -0500

    First pass at Teensy firmware to send button presses

---
+++ teensy_firmware/button/button.ino
@@ -0,0 +1,31 @@
+/*
+ * Button.pde
+ */
+
+#include <Bounce.h>
+// Create Bounce objects for each button.  The Bounce object
+// automatically deals with contact chatter or "bounce", and
+// it makes detecting changes very simple.
+Bounce button = Bounce(10, 10);
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(10, INPUT_PULLUP);
+  delay(1000);
+}
+
+void loop() {
+  // Update the button.  There should not be any long
+  // delays in loop(), so this runs repetitively at a rate
+  // faster than the buttons could be pressed and released.
+  button.update();
+  
+  if (button.fallingEdge()) {
+    Keyboard.println("button press");
+  }
+  if (button.risingEdge()) {
+    Keyboard.println("button release");
+  }
+}
+
